87a340fbe01ebcd7f739b5cd6db3973b9698da67
Nicholas Juntilla <worldnick@gmail.com>
2017-11-07 16:27:15 -0800

    name change

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -4,7 +4,7 @@ module.exports = (deployer) => {
   const totalTokens = 1000000000000000000;
   const tokenName = 'realTest';
   const numberOfDecimals = 9;
-  const tokenSymbol = "NickTest";
+  const tokenSymbol = "RT";
 
   deployer.deploy(HumanStandardToken, totalTokens, tokenName, numberOfDecimals, tokenSymbol)
 }
